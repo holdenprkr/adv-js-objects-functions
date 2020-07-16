b25ac25b197c466f496d62d57e412dda85f8d6a0
Holden Parker <holdenprkr@gmail.com>
2020-07-16 14:56:50 -0500

    covered IIFEs and closures

--- script.js
+++ script.js
@@ -87,70 +87,129 @@ change(age, obj);
 console.log(age); // 27
 console.log(obj.city); // San Francisco
 
-
-
 // Passing functions as arguments
 
 var years = [1996, 1965, 1937, 2005, 1998];
 
 function arrayCalc(arr, fn) {
-    var arrRes = [];
-    for (var i = 0; i < arr.length; i++) {
-        arrRes.push(fn(arr[i]))
-    }
-    return arrRes;
+  var arrRes = [];
+  for (var i = 0; i < arr.length; i++) {
+    arrRes.push(fn(arr[i]));
+  }
+  return arrRes;
 }
 
 function calculateAge(el) {
-    return 2020 - el;
+  return 2020 - el;
 }
 
 function isFullAge(el) {
-    return el >= 18;
+  return el >= 18;
 }
 
 function maxHeartRate(el) {
-    if (el >= 18 && el <= 81) {
-        return Math.round(206.9 - (.67 * el));
-    } else {
-        return -1;
-    }
+  if (el >= 18 && el <= 81) {
+    return Math.round(206.9 - 0.67 * el);
+  } else {
+    return -1;
+  }
 }
 
 var ages = arrayCalc(years, calculateAge);
 var fullAges = arrayCalc(years, isFullAge);
-var rates = arrayCalc(years, maxHeartRate)
+var rates = arrayCalc(years, maxHeartRate);
 console.log(ages);
 console.log(fullAges);
 console.log(rates);
 
-
-
 // Functions returning functions
 
 function interviewQuestion(job) {
-    if (job === 'designer') {
-        return function(name) {
-            console.log(name + ', can you please explain what UX design is?');
-        }
-    } else if (job === 'teacher') {
-        return function(name) {
-            console.log('What subject do you teach, ' + name + '?');
-        }
-    } else {
-        return function(name) {
-            console.log('Hello ' + name + ', what do you do?');
-        }
-    }
+  if (job === "designer") {
+    return function (name) {
+      console.log(name + ", can you please explain what UX design is?");
+    };
+  } else if (job === "teacher") {
+    return function (name) {
+      console.log("What subject do you teach, " + name + "?");
+    };
+  } else {
+    return function (name) {
+      console.log("Hello " + name + ", what do you do?");
+    };
+  }
 }
 
-var teacherQuestion = interviewQuestion('teacher');
-var designerQuestion = interviewQuestion('designer');
+var teacherQuestion = interviewQuestion("teacher");
+var designerQuestion = interviewQuestion("designer");
 
-teacherQuestion('John');
-designerQuestion('John');
-designerQuestion('Jane');
-designerQuestion('Mark');
-designerQuestion('Mike');
+teacherQuestion("John");
+designerQuestion("John");
+designerQuestion("Jane");
+designerQuestion("Mark");
+designerQuestion("Mike");
 
-interviewQuestion('teacher')('Mark');
+interviewQuestion("teacher")("Mark");
+
+// Imediately Invoked Function Expressions
+
+// Not an IIFE
+function game() {
+  var score = Math.random() * 10;
+  console.log(score >= 5);
+}
+game();
+
+// IIFE
+(function () {
+  var score = Math.random() * 10;
+  console.log(score >= 5);
+})();
+
+(function (goodLuck) {
+  var score = Math.random() * 10;
+  console.log(score >= 5 - goodLuck);
+})(5);
+
+
+
+// Closures
+
+function retirement(retirementAge) {
+  var a = ' years left until retirement.';
+  return function(yearOfBirth) {
+    var age = 2020 - yearOfBirth;
+    console.log((retirementAge - age) + a)
+  }
+}
+
+var retirementUS = retirement(66);
+retirementUS(1990);
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
+
+retirementUS(1990);
+retirementGermany(1990);
+retirementIceland(1990);
+
+// retirement(66)(1990);
+
+// Rewrite interview function as a closure challenge
+function interview(job) {
+  return function (name) {
+    switch (job) {
+      case "designer":
+        console.log(name + ", can you please explain what UX design is?")
+        break;
+      case "teacher":
+        console.log("What subject do you teach, " + name + "?");
+        break;
+      default:
+        console.log("Hello " + name + ", what do you do?")
+    }
+  } 
+}
+
+interview("designer")("David");
+interview("teacher")("Janis");
+interview("programmer")("Meghan");
